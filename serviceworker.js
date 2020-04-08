21eac1ff827b5afb063b1c6cba0898b250512283
Max1Truc <maxitruc.bonbec@gmail.com>
2020-04-08 23:56:26 +0200

    Fix a typo

--- serviceworker.js
+++ serviceworker.js
@@ -1,5 +1,5 @@
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    fetch(event.request).then((result) => result || new Response("<h1>Vous êtes hors-ligne</h1>");
+    fetch(event.request).then((result) => result || new Response("<h1>Vous êtes hors-ligne</h1>")
   );
 });
