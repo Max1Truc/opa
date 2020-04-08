4fec6b2d608940842811033f74e024fa72b9ca20
Max1Truc <maxitruc.bonbec@gmail.com>
2020-04-09 00:05:45 +0200

    I wasn't the latest

--- serviceworker.js
+++ serviceworker.js
@@ -1,5 +1,11 @@
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    fetch(event.request).then((result) => (result || new Response("<h1>Vous êtes hors-ligne</h1>")))
+    fetch(event.request).then((result) => {
+      return result || new Response("<h1>Vous êtes hors-ligne...</h1>", {
+        headers: {
+          'Content-Type': 'text/html'
+        }
+      });
+    })
   );
 });
