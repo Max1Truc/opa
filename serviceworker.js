ccdbab137319a2235e084d6261cf34d8d4bd8bb7
Max1Truc <maxitruc.bonbec@gmail.com>
2020-04-09 00:07:38 +0200

    Latest update, the serviceworker is currently useless

--- serviceworker.js
+++ serviceworker.js
@@ -1,11 +1,7 @@
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     fetch(event.request).then((result) => {
-      return result || new Response("<h1>Vous êtes hors-ligne...</h1>", {
-        headers: {
-          'Content-Type': 'text/html'
-        }
-      });
+      return result;
     })
   );
 });
