971b88e65f6c47e325f8d8a132359d2911b1bb1a
Max1Truc <maxitruc.bonbec@gmail.com>
2020-04-09 21:37:52 +0200

    Add offline support with serviceworker

---
+++ serviceworker.js
@@ -0,0 +1,29 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/offline/',
+        '/offline/offline.js',
+        '/offline/offline.css'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function(error) {
+      var url = new URL (event.request.url);
+      console.dir(url);
+      if (url.pathname == "/" || url.pathname == "/offline.js" || url.pathname == "/offline.css") {
+        url.pathname = "/offline" + url.pathname;
+        var request = new Request(url.href);
+        console.dir(request);
+        var cached = caches.match(request);
+        return cached;
+      } else {
+        return error;
+      }
+    })
+  );
+});
